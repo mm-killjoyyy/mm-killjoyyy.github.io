f274dd7a97f5673d2d6d081084e8f18b3372ce82
Monica <monica.munoz17@yahoo.com>
2019-05-06 17:34:21 -0700

    failure

---
+++ sketch.js
@@ -0,0 +1,13 @@
+// function preload () {
+//   img = loadImage("blueberry.png");
+// }
+//
+// function setup() {
+//   createCanvas(1200, 600);
+// }
+//
+// function draw() {
+//   background(197);
+//   image(img, mouseX -20, mouseY-10);
+//
+// }
